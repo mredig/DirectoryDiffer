9ba573eaa817ff9681e6bb5681a0c4222c73d563
Michael Redig <mredig@gmail.com>
2024-04-29 21:31:29 -0500

    (refactor) delete some dead code

--- Sources/DirectoryDifferCore/DirectoryDifferCore.swift
+++ Sources/DirectoryDifferCore/DirectoryDifferCore.swift
@@ -192,66 +192,6 @@ public enum DirectoryDifferCore {
 		return out.merging(commonResults).sorted()
 	}
 
-	private static func duplicates(
-		betweenSourceContent sourceContents: [URL],
-		andDestinationContent destinationContent: [URL],
-		comparingHashes: Bool
-	) async throws -> [URL] {
-
-		func key(_ keyValue: String) -> String {
-			comparingHashes ? keyValue.lowercased() : keyValue
-		}
-
-		let destinationNames = destinationContent.reduce(into: [String: URL](), {
-			$0[key($1.lastPathComponent)] = $1
-		})
-		let matches = sourceContents.filter { destinationNames[key($0.lastPathComponent)] != nil }
-
-		guard comparingHashes else { return matches }
-
-		let hashMatches = try await withThrowingTaskGroup(of: URL?.self) { group in
-			let queue = SchedulingQueue<URL?>()
-			for (index, sourceURL) in matches.enumerated() {
-				guard let destinationURL = destinationNames[key(sourceURL.lastPathComponent)] else { continue }
-				group.addTask {
-					return try await queue.addTask(label: "\(index): \(sourceURL.lastPathComponent)") {
-						print("Comparing files named \(sourceURL.lastPathComponent)...")
-						let sourceIsDirectory = try sourceURL.resourceValues(forKeys: [.isDirectoryKey])
-						let destinationIsDirectory = try destinationURL.resourceValues(forKeys: [.isDirectoryKey])
-						if
-							sourceIsDirectory.isDirectory.nilIsFalse == true,
-							destinationIsDirectory.isDirectory.nilIsFalse == true {
-
-							print("\(sourceURL.lastPathComponent) match!")
-							return sourceURL
-						} else if sourceIsDirectory.isDirectory.nilIsFalse != destinationIsDirectory.isDirectory.nilIsFalse {
-
-							print("Directory/file mismatch: \(sourceURL)")
-							return nil
-						}
-
-						async let sourceHash = Insecure.MD5.hash(sourceURL)
-						async let destinationHash = Insecure.MD5.hash(destinationURL)
-
-						guard try await sourceHash == (try await destinationHash) else { return nil }
-						print("\(sourceURL.lastPathComponent) match!")
-						return sourceURL
-					}
-				}
-			}
-
-			var matchingURLs: [URL] = []
-			for try await matchingURL in group {
-				guard let matchingURL else { continue }
-				matchingURLs.append(matchingURL)
-			}
-			return matchingURLs
-		}
-		print()
-
-		return hashMatches
-	}
-
 	public typealias HashDigest = Insecure.MD5Digest
 	public struct DiffResults {
 		public internal(set) var identical: [[String]] = []
