fb3c3882687253fc3313abe29fc040ff38c4a298
Michael Redig <mredig@gmail.com>
2024-04-29 21:12:27 -0500

    (feat) added duple

---
+++ Sources/DirectoryDifferCore/Duple.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public struct Duple<T> {
+	let source: T
+	let destination: T
+}
+
+extension Duple: Comparable where T: Comparable {
+	public static func < (lhs: Duple<T>, rhs: Duple<T>) -> Bool {
+		guard lhs.source != rhs.source else {
+			return lhs.source < rhs.source
+		}
+		return lhs.destination < rhs.destination
+	}
+}
+extension Duple: Hashable where T: Hashable {}
+extension Duple: Equatable where T: Equatable {}
